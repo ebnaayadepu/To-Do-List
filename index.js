22ad19f8fe38528360c39125dee64ad8f69bbe35
ebnaayadepu <ebnaepu@gmail.com>
2025-02-02 22:03:07 +0600

    Show alert & add to do

--- index.js
+++ index.js
@@ -2,3 +2,15 @@ const todoInput = document.querySelector("#todo-input");
 const addButton = document.querySelector("#add-button");
 const todoList =document.querySelector("#todo-list");
 
+//add to-do item
+
+addButton.addEventListener("click", () => {
+   const todoText = todoInput.value.trim();
+   if(todoText === "") {
+    alert('Input field is empty. Please enter a to-do item.');
+    return;
+   
+   }
+   addTodoItem(todoText);
+   todoInput.value = "";
+})
